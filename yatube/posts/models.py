bcf9a00b883e67cc46ddbe76d6da8699cb0011c9
Роман Шпагин <foxrage228@mail.ru>
2022-10-19 16:01:25 +0400

    1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,17 +6,20 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Заголовок')
-    slug = models.SlugField(unique=True, max_length=50)
-    description = models.TextField()
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    slug = models.SlugField(verbose_name='Имя', unique=True, max_length=50,)
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        
 
 class Post(models.Model):
-    text = models.TextField(max_length=200, verbose_name='Текст')
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+    text = models.TextField(verbose_name='Текст', max_length=200)
+    pub_date = models.DateTimeField(verbose_name='Дата', auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -33,9 +36,9 @@ class Post(models.Model):
         verbose_name='Группа')
 
 
-class Meta:
-    verbose_name_plural = 'Посты'
-    ordering = ('-pub_date',)
+    class Meta:
+        verbose_name_plural = 'Посты'
+        ordering = ('-pub_date',)
 
 
 def __str__(self):
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
+
+from django.shortcuts import get_object_or_404, render
+
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post = Post.objects.all()[:10]
     context = {
-        'posts': posts,
+        'posts': post,
     }
     return render(request, 'posts/index.html', context)
 
-
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:10]
     context = {
         'group': group,
         'posts': posts,
